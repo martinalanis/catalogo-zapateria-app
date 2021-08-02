c23a1a36426a408f0249211a18fb888dfbe6cb62
martinalanis <martinalanis.dev@gmail.com>
2021-08-02 14:29:45 -0500

    feat: Barra de busqueda por codigo en listado

--- pages/categorias/_categoria.vue
+++ pages/categorias/_categoria.vue
@@ -12,8 +12,8 @@
       </div>
     </div>
     <v-container>
-      <v-row class="justify-center justify-md-start">
-        <v-col cols="12" sm="6" md="3">
+      <v-row class="justify-start">
+        <v-col :cols="envType !== 'client' ? 5 : 6" md="3">
           <v-select
             v-model="tipo"
             :items="tipos"
@@ -26,6 +26,30 @@
             color="#Pro"
             @change="updateType"
           />
+        </v-col>
+        <v-col v-if="envType !== 'client'" cols="7" sm="6" md="3">
+          <v-form lazy @submit.prevent="doSearch">
+            <v-text-field
+              v-model="search"
+              label="Codigo"
+              background-color="#fafafa"
+              hide-details
+              clearable
+              solo
+              dense
+              @click:clear="updateType"
+            >
+              <v-btn
+                slot="append"
+                icon
+                type="submit"
+              >
+                <v-icon>
+                  mdi-magnify
+                </v-icon>
+              </v-btn>
+            </v-text-field>
+          </v-form>
         </v-col>
       </v-row>
       <v-row class="mb-6 relative" :class="{ 'overlay-loading': loading }">
@@ -80,12 +104,18 @@ export default {
         total: res.total,
         categoria,
         headerImage,
-        loading: false,
-        tipos,
-        tipo: ''
+        tipos
       }
     } catch (err) {
       error({ statusCode: err.response.status, message: err.response.statusText })
+    }
+  },
+  data () {
+    return {
+      search: '',
+      envType: process.env.type,
+      tipo: '',
+      loading: false
     }
   },
   mounted () {
@@ -102,9 +132,12 @@ export default {
       try {
         if (this.currentPage < this.lastPage) {
           this.loading = true
-          const endPoint = this.tipo
+          let endPoint = this.tipo
             ? `/products/${this.categoria}/all?page=${this.currentPage + 1}&type=${this.tipo}`
             : `/products/${this.categoria}/all?page=${this.currentPage + 1}`
+          if (this.search) {
+            endPoint += `&search=${this.search}`
+          }
           const res = await this.$axios.$get(endPoint)
           this.currentPage = res.current_page
           this.shoes = [...this.shoes, ...res.data]
@@ -113,6 +146,24 @@ export default {
         console.log(err)
       } finally {
         this.loading = false
+      }
+    },
+    async doSearch () {
+      if (this.search) {
+        try {
+          this.loading = true
+          this.tipo = ''
+          const endPoint = `/products/${this.categoria}/all?search=${this.search}`
+          const res = await this.$axios.$get(endPoint)
+          this.currentPage = res.current_page
+          this.lastPage = res.last_page
+          this.total = res.total
+          this.shoes = res.data
+        } catch (err) {
+          console.log(err)
+        } finally {
+          this.loading = false
+        }
       }
     },
     async updateType () {
